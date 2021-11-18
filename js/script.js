2db581c1b9ff0a13bfa843d9fdbfc397b24f8caa
TinchoCode <89424821+TinchoCode@users.noreply.github.com>
2021-11-18 13:16:51 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+  document.getElementById("myDropdown").classList.toggle("show");
+}
   
-  window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
     }
   }
+}
+
+function showCosas(noVer, ver){
+  var elemento = document.getElementsByClassName(noVer);
+  for(i=0; i<elemento.length; i++){elemento[i].style.display = 'none'}
+  var elemento2 = document.getElementsByClassName(ver);
+  for(i=0; i<elemento2.length; i++){elemento2[i].style.display = 'block'}
+
+}
+

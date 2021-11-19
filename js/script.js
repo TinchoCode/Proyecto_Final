bba19a2fae786f39b2134676e08ab8120570c919
TinchoCode <89424821+TinchoCode@users.noreply.github.com>
2021-11-19 16:19:42 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,3 +19,18 @@ function showCosas(noVer, ver){
 
 }
 
+function tikiTaka(invocoA){
+  var v = document.getElementsByClassName('viendo');
+  var invocado = document.getElementsByClassName(invocoA);
+  for(i=0; i<v.length; i++){
+    v[i].style.display = 'none'
+    v[i].classList.remove('viendo');
+    for(j=0; j<invocado.length; j++){
+      invocado[i].style.display = 'block'
+      invocado[i].classList.remove('escond');
+      invocado[i].classList.add('viendo');
+    }
+    v[i].classList.add('escond');
+  }
+}
+

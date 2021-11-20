56d924f8ebb736e75ec5e180f6a4ad68ceca6ebd
TinchoCode <89424821+TinchoCode@users.noreply.github.com>
2021-11-20 15:21:55 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-  
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
-}
-
-function showCosas(noVer, ver){
-  var elemento = document.getElementsByClassName(noVer);
-  for(i=0; i<elemento.length; i++){elemento[i].style.display = 'none'}
-  var elemento2 = document.getElementsByClassName(ver);
-  for(i=0; i<elemento2.length; i++){elemento2[i].style.display = 'block'}
-
-}
-
 function tikiTaka(invocoA){
   var v = document.getElementsByClassName('viendo');
   var invocado = document.getElementsByClassName(invocoA);
@@ -33,4 +12,3 @@ function tikiTaka(invocoA){
     v[i].classList.add('escond');
   }
 }
-
